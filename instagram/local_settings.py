4569c6b31936afd21e00e4c474dd170a7e6d4fe6
Ali Miri <miri03369@gmail.com>
2025-09-04 17:11:11 +0330

    add models

---
+++ instagram/local_settings.py
@@ -0,0 +1,12 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-f9uftht2@m)hy%x369v7z7-(x6_8ir1&+4br!)7xbrqu5y6=ex'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+DB_NAME = 'instagram'
+DB_USER = "postgres"
+DB_PASSWORD = 1234
+DB_HOST = "localhost"
+DB_PORT = 5432
