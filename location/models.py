5e613e751554515cbf7747858c5fe9b9c8fc729e
Ali Miri <miri03369@gmail.com>
2025-09-04 17:33:12 +0330

    add location models

---
+++ location/models.py
@@ -0,0 +1,18 @@
+
+from django.db import models
+from lib.common_models import BaseModel
+from django.utils.translation import gettext_lazy as _
+
+# from django.contrib.postgres.fields import JSONField
+
+
+class Location(BaseModel):
+    title = models.CharField(_("title"), max_length=32)
+    points = models.JSONField(_("points"))
+
+    class Meta:
+        verbose_name = _("Location")
+        verbose_name_plural = _("Locations")
+
+    def __str__(self):
+        return self.title
