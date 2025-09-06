2613abd5a9593d4182cda785d9392a8e67ce05f8
Ali Miri <miri03369@gmail.com>
2025-09-06 19:11:45 +0330

    add content admin

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from content.models import Post, PostMedia, Tag, PostTag, TaggedUser
 
-# Register your models here.
+@register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('caption', 'user', 'location')
+
+
+@register(PostMedia)
+class PostMediaAdmin(admin.ModelAdmin):
+    list_display = ("media_type",)
+
+
+
+
+
+
