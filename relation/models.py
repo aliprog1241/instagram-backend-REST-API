76fbf8626815a7f7feda5ef33a13bfa2a60c710b
Ali Miri <miri03369@gmail.com>
2025-09-06 17:55:04 +0330

    creat relations

--- relation/models.py
+++ relation/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from lib.commen_models import BaseModel
+
+User = get_user_model()
+
