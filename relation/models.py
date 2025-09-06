37526c5a335125779b35ef2b226dc04be0475f47
Ali Miri <miri03369@gmail.com>
2025-09-06 18:13:22 +0330

    creat activity likes comment and ...

--- relation/models.py
+++ relation/models.py
@@ -6,3 +6,14 @@ from lib.commen_models import BaseModel
 
 User = get_user_model()
 
+
+class Relation(BaseModel):
+    from_user = models.ForeignKey(User, related_name='followings',on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='followers',on_delete=models.CASCADE)
+
+    class Meta:
+            verbose_name = _("Relation")
+            verbose_name_plural = _("Relations")
+
+    def __str__(self):
+        return "{} >> {}".format(self.from_user.username, self.to_user.username)
