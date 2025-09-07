b70cafb06efecd3e579e560b7a4ad16400329cf9
Ali Miri <miri03369@gmail.com>
2025-09-07 17:38:31 +0330

    clean code

--- content/serializers.py
+++ content/serializers.py
@@ -2,19 +2,34 @@ from rest_framework import serializers
 
 from content.models import Tag
 
+#
+# class TagListSerializer(serializers.Serializer):
+#     title = serializers.CharField()
+#     id = serializers.IntegerField(read_only=True)
+#
+#     def create(self, validated_data):
+#         instance = Tag.objects.create(**validated_data)
+#         return instance
+#
+# class TagDetailSerializer(TagListSerializer):
+#     posts = serializers.SerializerMethodField()
+#
+#
+#     def get_posts(self, obj):
+#
+#         return obj.posts.count()
 
-class TagListSerializer(serializers.Serializer):
-    title = serializers.CharField()
-    id = serializers.IntegerField(read_only=True)
+class TagListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id','title')
 
-    def create(self, validated_data):
-        instance = Tag.objects.create(**validated_data)
-        return instance
 
-class TagDetailSerializer(TagListSerializer):
+class TagDetailSerializer(serializers.ModelSerializer):
     posts = serializers.SerializerMethodField()
-
+    class Meta:
+        model = Tag
+        fields = ('id','title')
 
     def get_posts(self, obj):
-
         return obj.posts.count()
