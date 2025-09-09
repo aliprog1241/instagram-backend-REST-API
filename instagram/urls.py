a1e6672dfe16d3c2117ecedfb6c37c5ed3fb4d72
Ali Miri <miri03369@gmail.com>
2025-09-09 15:34:28 +0330

    install JWT

--- instagram/urls.py
+++ instagram/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from content.views import TagDetailApi
 
@@ -23,6 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('content/', include('content.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
 ]
